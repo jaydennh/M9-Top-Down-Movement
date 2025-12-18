928ef8050902e8bb53490742069d9ee0a0a83769
Jayden Huang <jahuang2008@gmail.com>
2025-12-18 12:26:49 -0800

    M9L11

--- lessons/pause_screen.gd
+++ lessons/pause_screen.gd
@@ -1,14 +1,24 @@
 @tool
 extends Control
 
+@export_range(0.1, 10.0, 0.01, "or_greater") var animation_duration := 2.3
+var _tween: Tween
+var _is_currently_opening := false
+
 @onready var _blur_color_rect: ColorRect = %BlurColorRect
 @onready var _ui_panel_container: PanelContainer = %UIPanelContainer
+@onready var _resume_button: Button = %ResumeButton
+@onready var _quit_button: Button = %QuitButton
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+	if Engine.is_editor_hint():
+		return
+	menu_opened_amount = 0.0
+	_resume_button.pressed.connect(toggle)
+	_quit_button.pressed.connect(get_tree().quit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -26,3 +36,26 @@ func set_menu_opened_amount(amount: float) -> void:
 	_blur_color_rect.material.set_shader_parameter("saturation", lerp(1.0, 0.3, amount))
 	_blur_color_rect.material.set_shader_parameter("tint_strength", lerp(0.0, 0.2, amount))
 	_ui_panel_container.modulate.a = amount
+	if not Engine.is_editor_hint():
+		get_tree().paused = amount > 0.3
+
+func toggle() -> void:
+	_is_currently_opening = not _is_currently_opening
+	
+	var duration := animation_duration
+	
+	if _tween != null:
+		if not _is_currently_opening:
+			duration = _tween.get_total_elapsed_time()
+		_tween.kill()
+	
+	_tween = create_tween()
+	_tween.set_ease(Tween.EASE_OUT)
+	_tween.set_trans(Tween.TRANS_QUART)
+	
+	var target_amount := 1.0 if _is_currently_opening else 0.0
+	_tween.tween_property(self, "menu_opened_amount", target_amount, duration)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		toggle()
